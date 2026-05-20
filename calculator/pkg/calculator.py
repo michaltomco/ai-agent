bb0dddbaa52134e4ac1316e9b53a7002e177e20f
Michal Tomčo <tomco.mito@gmail.com>
2026-05-20 21:02:31 +0200

    Add file operation helpers and tests

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -51,7 +51,7 @@ class Calculator:
 
         return values[0]
 
-    def _apply_operator(self, operators: list[str], values: list[float]) -> None:
+    def _apply_operator(self, operators, values):
         if not operators:
             return
 
